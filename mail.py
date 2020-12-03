53494ed83517e69e64604a36ac76666725cd9c4b
bautista allende <bautiallende@hotmail.com>
2020-12-03 15:31:16 +0100

    subiendo modificaciones

--- mail.py
+++ mail.py
@@ -1,10 +1,7 @@
 from flask import Flask, render_template, url_for, request, redirect
 app = Flask(__name__)
 import csv
-import smtplib
-from email.message import EmailMessage
-from string import Template
-from pathlib import Path
+
 
 
 
